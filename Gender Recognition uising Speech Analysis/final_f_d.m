959fafb366a32e1dd9677b395f23895d657e39c6
Avinash Ummagani <44708962+AvinashUmmagani@users.noreply.github.com>
2020-05-28 14:07:24 +0530

    Final Upload

---
+++ Gender Recognition uising Speech Analysis/final_f_d.m
@@ -0,0 +1,38 @@
+clear;
+clc;
+
+filename= 'wav\female\arctic_a0035.wav';
+
+[y,Fs]= audioread(filename);
+N=length(y);
+
+sound(y,Fs);
+
+%plot the input signal
+t = linspace(0,N/Fs,N);
+figure(1);
+plot(t,y);
+
+%FFT of the voice signal
+FFTy=fft(y,N);
+w=Fs*linspace(0,1,N);
+figure(2);
+plot(w,abs(FFTy))
+axis([0 8000 0 max(abs(FFTy))])
+
+%finding freq where the maximun occurs
+[val,ind]=max(FFTy(1:N/2));
+
+fo=ind*Fs/N;
+
+disp('maximum occured at freq=')
+disp(fo)
+
+%decision making
+if(fo<168)
+    disp('THE VOICE IS OF A MALE');
+else
+    disp('THE VOICE IS OF A FEMALE');
+end
+
+
